233f033087e77eb299c2068450ddf12235089ada
jt1992@gmail.com <jt1992@gmail.com>
2018-06-14 20:12:34 +0800

    add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,6 @@
 const socket = io();
+const messageForm = document.querySelector('#message-form');
+const messages = document.querySelector('#messages');
 
 socket.on('connect', () => {
   console.log('Connected to server');
@@ -6,8 +8,31 @@ socket.on('connect', () => {
 
 socket.on('newMessage', (message) => {
   console.log(message);
+  const newMessage = document.createElement('li');
+
+  newMessage.textContent = `${message.from}: ${message.text}`;
+  messages.appendChild(newMessage);
 });
 
 socket.on('disconnect', () => {
   console.log('Disconnected from server');
+});
+
+socket.emit('createMessage', {
+  from: 'Me',
+  text: 'it\'s meee'
+}, (message) => {
+  console.log(message);
+});
+
+messageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const text = document.querySelector('[name=message]').value;
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text
+  }, (data) => {
+    console.log(data);
+  });
 });
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const { generateMessage } = require('./utils/message');
+
 const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV || 'development';
 const app = express();
@@ -16,33 +18,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the fight',
-    createdAt: new Date().getTime()
-  });
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'A new challenger has appeared',
-    createdAt: new Date().getTime()
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the fight'));
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'A new challenger has appeared'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log(message);
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback(message.text);
   });
 
   socket.on('disconnect', () => {
     console.log('User disconnected');
-    socket.broadcast.emit('newMessage', {
-      from: 'Admin',
-      text: 'A hero has been vanquished',
-      createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'A hero has fallen'));
   });
 });
 
