55ef9fe465190fe790f52b9732a552cc1cd877bd
jt1992@gmail.com <jt1992@gmail.com>
2018-06-14 17:14:48 +0800

    Added newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -15,6 +15,26 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
+
+  // socket.emit('newEmail', {
+  //   from: 'JT',
+  //   body: 'Yo dude',
+  //   createdAt: 123
+  // });
+  socket.emit('newMessage', {
+    from: 'JT',
+    body: 'Yo dude',
+    createdAt: new Date()
+  });
+
+  socket.on('newMessage', (message) => {
+    console.log(message);
+  });
+
+  // socket.on('newEmail', (email) => {
+  //   console.log(email);
+  // });
+
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
