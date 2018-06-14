82283a94a14dbdb7bf01ef621c77b7a325b389db
jt1992@gmail.com <jt1992@gmail.com>
2018-06-14 18:43:16 +0800

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -16,17 +16,33 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the fight',
+    createdAt: new Date().getTime()
+  });
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'A new challenger has appeared',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
     console.log(message);
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
-      createdAt: new Date()
+      createdAt: new Date().getTime()
     });
   });
 
   socket.on('disconnect', () => {
     console.log('User disconnected');
+    socket.broadcast.emit('newMessage', {
+      from: 'Admin',
+      text: 'A hero has been vanquished',
+      createdAt: new Date().getTime()
+    });
   });
 });
 
